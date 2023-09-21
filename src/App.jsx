cdb031d41049b11f30e6b0cb7e3d193db5f79b07
Hugo Oyola <hugo.ham@outlook.com>
2023-09-21 16:28:50 -0500

    Se creo la lista de Criptomonedas, se agrego Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  const [coins, setCoins] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}assets`)
+      .then((data) => {
+        setCoins(data.data.data);
+        // console.log(data);
+      })
+      .catch(() => {
+        console.error("Error en la petición");
+      });
+  }, []);
+
+  if (!coins) return <span>Cargando...</span>;
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Lista de Criptomonedas</h1>
+      <ol>
+        {coins.map(({ id, name, symbol }) => (
+          <li key={id}>
+            Nombre: {name} - {symbol}
+          </li>
+        ))}
+      </ol>
     </>
   );
 }
